0ae6c4f3177049e5d8d3473c5752d655b7426db2
root <renbin.guo>
2018-08-10 16:37:38 +0800

    add comment for 14.c

--- 14_longest-common-prefix.c
+++ 14_longest-common-prefix.c
@@ -1,6 +1,24 @@
+/*
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+
+示例 1:
+
+输入: ["flower","flow","flight"]
+输出: "fl"
+示例 2:
+
+输入: ["dog","racecar","car"]
+输出: ""
+解释: 输入不存在公共前缀
+*/
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
+/*
+	brief : find the shortest len string from strs,and return the index 
+*/
 int get_min_array_index(char** strs, int strsSize){
 	int i ;
 	int min_len = strlen(strs[0]);
@@ -13,6 +31,12 @@ int get_min_array_index(char** strs, int strsSize){
 	}
 	return min_len_index;
 }
+/*
+	算法思路：
+	先找出所有输入字符串中最短的那个设为shortest_str,
+	然后对shortest_str中的每个字符，在所有strs中查看是否都有
+	所有是一个二层for遍历.
+*/
 char* longestCommonPrefix(char** strs, int strsSize){
     int i,j ;
     printf("strlen(strs[0])  = %d\n",strlen(strs[0]));
