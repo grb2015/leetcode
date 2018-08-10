bc95a034e73a58f296d712468b847f50e8bb5525
root <renbin.guo>
2018-08-10 22:54:43 +0800

    add 20_valid-parentheses.py

--- 20_valid-parentheses.py
+++ 20_valid-parentheses.py
@@ -28,35 +28,49 @@
 输入: "{[]}"
 输出: true
 '''
+
+'''
+每当遇到一个右侧符号时，检查栈是否为空(右括号不入栈)，若此时栈不为空.
+则对栈进行pop操作表明顶部元素已被匹配，否则为不匹配情况.
+直接返回false .当整个字符串遍历结束，我们就可以通过判断该栈是否为空来判断整个字符串中的符号是否匹配
+'''
 class Solution:
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
+        if(len(s) == 0):  ## for ""
+        	return True
         dict_prths = {'(':')','[':']','{':'}'}  # dict parentheses
         s_left  = []  # stored all the left parenthes in s 
         s_right = []  # right 
+
         for c in s:
-        	if c in dict_prths.keys():
-        		s_left.append(c)
-        	elif c in dict_prths.values():
-        		s_right.append(c)
+        	if c not in dict_prths.keys() and c not in dict_prths.values():
+        		return False
+       	stack = []   # python中栈可以就是list 
+        for i in range(len(s)):
+        	if s[i] in dict_prths.keys():
+        		stack.append(s[i])
+        	elif  s[i] in dict_prths.values():
+        		if len(stack) > 0:
+        			if dict_prths[stack.pop()] != s[i]:
+        				return False
+        		else: ## for "))"
+        			return False
         	else:
-        		print("invaid input ")
         		return False
-        if len(s_left) != len(s_right):
-        	return False
-        for i in range(len(s_left)):
-        	if dict_prths[s_left[i]] != s_right[i]:
-        		return False
-        return True 
-if __name__ == '__main__':
-	s = Solution()
-	print(s.isValid("()") )  # True
-	print(s.isValid("()[]{}")) # True
-	print(s.isValid("(]"))    #False
-	#这种算法是有问题，处理这两个用例会出错
-	print(s.isValid("([)]")) # False   
-	print(s.isValid("{[]}")) # True   
+        return False if  len(stack) > 0  else  True  ## for "((" 
+s = Solution()
+print(s.isValid("()") )  # True
+print(s.isValid("()[]{}")) # True
+print(s.isValid("(]"))    #False
+print(s.isValid("([)]")) # False   
+print(s.isValid("{[]}")) # True   
+print(s.isValid("[")) # False   
+print(s.isValid("")) # True  
+print(s.isValid("((")) # False  
+print(s.isValid("}}")) # False  
+print(s.isValid("}}ddd")) # Flase  
 
