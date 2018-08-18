086039f0774f996c8f6663448a07589c1cfb352d
renbin.guo <949409706@qq.com>
2018-08-18 18:29:58 +0800

    实现图的深度优先和广度优先遍历

---
+++ data_struct/BFS.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# @Author: Teiei
+# @Date:   2018-08-18 17:41:46
+# @Last Modified by:   Teiei
+# @Last Modified time: 2018-08-18 18:28:32
+from Graph import Graph
+def BFS(G,start):
+	'''[summary] 图的宽度优先遍历
+	
+	[description]
+	
+	Arguments:
+		G {[class Graph ]} -- [要遍历的图]
+		start {[int]} -- [起始节点]
+	''' 
+	queue = []
+	queue.append(start)
+	G.set_mark(start)
+	while(len(queue) != 0):
+		# dequeue
+		v = queue[0]
+		print(v)
+		queue = queue[1:]
+		t = G.first(v)
+		while(t != -1):
+			if(G.mark[t] == "UNVISTED"):
+				G.set_mark(t)
+				queue.append(t)
+			t = G.next(v,t)
+if __name__ == '__main__':
+	G = Graph(5)
+	G.set_edge(0, 1, 1)
+	G.set_edge(0, 4, 1)
+	G.set_edge(1, 3, 1)
+	G.set_edge(2, 4, 1)
+	G.set_edge(3, 2, 1)
+	G.set_edge(4, 1, 1)
+	G.print_graph()
+	BFS(G,0)
---
+++ data_struct/DFS.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @Author: Teiei
+# @Date:   2018-08-18 15:29:23
+# @Last Modified by:   Teiei
+# @Last Modified time: 2018-08-18 17:36:13
+
+from Graph import Graph
+
+
+def DFS(G, v):
+	'''[summary] deep first trave for a Graph
+
+	[description]
+
+	Arguments:
+		G {[Graph]} -- [图的数据结构]
+		v {[int]} -- [起始节点]
+	'''
+	G.set_mark(v)
+	print(v)
+	t = G.first(v)
+	while(t != -1):
+		if(G.mark[t] == "UNVISTED"):
+			DFS(G,t)
+		t = G.next(v,t)
+	
+
+if __name__ == '__main__':
+	G = Graph(5)
+	G.set_edge(0, 1, 1)
+	G.set_edge(0, 4, 1)
+	G.set_edge(1, 3, 1)
+	G.set_edge(2, 4, 1)
+	G.set_edge(3, 2, 1)
+	G.set_edge(4, 1, 1)
+	G.print_graph()
+	DFS(G,0)
